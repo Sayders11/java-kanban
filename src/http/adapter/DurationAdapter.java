1e981510ed67ef5669b4da5f860c79c811e4bd0c
Makhoro <sayders110696@gmail.com>
2024-08-04 22:00:09 +0300

    Исправлена часть недочетов

--- src/http/adapter/DurationAdapter.java
+++ src/http/adapter/DurationAdapter.java
@@ -3,7 +3,6 @@ package http.adapter;
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
-import exceptions.ManagerSaveException;
 
 import java.io.IOException;
 import java.time.Duration;
@@ -13,10 +12,10 @@ public class DurationAdapter extends TypeAdapter<Duration> {
 
     @Override
     public void write(JsonWriter jsonWriter, Duration duration) throws IOException {
-        try {
+        if (duration == null) {
+            jsonWriter.nullValue();
+        } else {
             jsonWriter.value(duration.toMinutes());
-        } catch (NullPointerException e) {
-            throw new ManagerSaveException();
         }
     }
 
--- src/http/adapter/LocalDateAdapter.java
+++ src/http/adapter/LocalDateAdapter.java
@@ -14,7 +14,11 @@ public class LocalDateAdapter extends TypeAdapter<LocalDateTime> {
 
     @Override
     public void write(final JsonWriter jsonWriter, final LocalDateTime localDateTime) throws IOException {
-        jsonWriter.value(localDateTime.format(FORMAT));
+        if (localDateTime == null) {
+            jsonWriter.nullValue();
+        } else {
+            jsonWriter.value(localDateTime.format(FORMAT));
+        }
     }
 
     @Override
